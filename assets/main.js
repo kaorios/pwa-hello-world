5d7fdd8a93091ee7a73b056494c0dc367f5644c4
kaori <suige787@live.jp>
2019-06-22 15:59:06 -0700

    とりあえずやったことコミット

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,141 @@
-/* eslint-disable no-use-before-define */
-
 async function installServiceWorker () {
-  // TODO
-}
-
-async function main () {
-  // sometimes accidentally open '/' while developing
-  if (!window.location.pathname.startsWith('/pwa-hello-world/')) {
-    window.location.replace('/pwa-hello-world/');
-  }
+  const reg = await navigator.serviceWorker.register('./service-worker.js');
+  console.log('[SW] Register', reg);
 }
 
 installServiceWorker();
-document.addEventListener('DOMContentLoaded', main);
+
+navigator.serviceWorker.addEventListener('message', async (event) => {
+  const message = event.data;
+  if (message.type === 'ping') {
+    console.log(`[main] ${message.text}`);
+  }
+});
+
+navigator.serviceWorker.addEventListener('message', async (event) => {
+  const message = event.data;
+  switch (message.type) {
+    case 'sw/install': {
+      // eslint-disable-next-line no-alert
+      const ok = window.confirm('New version is available. Reload now?');
+      if (ok) {
+        window.location.reload();
+      }
+      break;
+    }
+
+    default: // do nothing
+  }
+});
+
+/**
+ * @param {number} ms
+ */
+function sleep (ms) {
+  return new Promise((f) => setTimeout(f, ms));
+}
+
+async function main () {
+  const reg = await navigator.serviceWorker.ready;
+  if (reg.waiting) {
+    await sleep(500); // just in case when infinite loop occurs accidentally
+
+    const message = { type: 'sw/skipWaiting' };
+    reg.waiting.postMessage(message);
+    window.location.reload();
+
+    return;
+  }
+
+  console.log('[main] Ready!');
+}
+
+main();
+
+/**
+ * @param {HTMLInputElement} el
+ * @param {boolean} enabled
+ */
+function turnNotification (el, enabled) {
+  const { permission } = Notification;
+  if (permission === 'granted') {
+    // eslint-disable-next-line no-param-reassign
+    el.checked = enabled;
+  } else if (permission === 'denied') {
+    // eslint-disable-next-line no-param-reassign
+    el.checked = false;
+
+    // eslint-disable-next-line no-alert
+    window.alert(
+      'You have denied push Notification. You have to update your decision.',
+    );
+  } else {
+    // eslint-disable-next-line no-param-reassign
+    el.checked = false;
+
+    Notification.requestPermission((newPermission) => {
+      if (newPermission === 'default') {
+        // eslint-disable-next-line no-alert
+        window.alert(
+          'You may have to reload in order to update your decision.',
+        );
+      } else {
+        turnNotification(el, enabled);
+      }
+    });
+  }
+}
+
+/** @type {HTMLInputElement} */
+const elNotificationEnabled = document.querySelector('#notificationEnabled');
+elNotificationEnabled.addEventListener('click', () => {
+  const enabled = elNotificationEnabled.checked;
+  turnNotification(elNotificationEnabled, enabled);
+});
+
+/**
+ * @param {string} body
+ */
+function showNotification (body) {
+  if (Notification.permission !== 'granted') {
+    return;
+  }
+
+  const title = 'PWA';
+  /** @type {NotificationOptions} */
+  const options = {
+    body,
+    icon: '/pwa-hello-world/assets/gpui/icon-512.png',
+  };
+  const notification = new Notification(title, options);
+  console.log('notification', notification);
+}
+
+/** @type {HTMLButtonElement} */
+const elShowNotification = document.querySelector('#showNotification');
+elShowNotification.addEventListener('click', () => {
+  showNotification('Hello!PWA');
+});
+
+// async function getPushEndpoint () {
+//   const reg = await navigator.serviceWorker.ready;
+//   const sub = await reg.pushManager.getSubscription();
+//   if (sub) {
+//     return sub.endpoint;
+//   }
+
+//   const newSub = await reg.pushManager.subscribe({
+//     userVisibleOnly: true,
+//   });
+//   // eslint-disable-next-line no-console
+//   console.log('End point (new)', newSub.endpoint);
+//   return newSub.endpoint;
+// }
+
+// getPushEndpoint().then((endpoint) => {
+//   const serverKey = 'XXXX';
+//   const command = `curl "${endpoint}" \
+//   --request POST --header "TTL: 60" --header "Content-Length: 0" \
+//   --header "Authorization: key=${serverKey}"`;
+//   console.log(command);
+// });
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,101 @@
-// TODO
+/** @type {ServiceWorkerGlobalScope} */
+// @ts-ignore
+// eslint-disable-next-line no-restricted-globals
+const sw = self;
+
+async function updateCache () {
+  const cache = await sw.caches.open('pwa-hello-world');
+  await cache.addAll([
+    '/pwa-hello-world/',
+  ]);
+}
+
+sw.addEventListener('install', (event) => {
+  console.log('[SW] Install!');
+  const p = updateCache()
+    .then(async () => {
+      const clients = await sw.clients.matchAll({ includeUncontrolled: true });
+      clients.forEach((client) => {
+        const message = { type: 'sw/install' };
+        client.postMessage(message);
+      });
+    });
+  event.waitUntil(p);
+});
+
+/**
+ * @param {string} body
+ */
+function showNotification (body) {
+  const title = 'PWA';
+  /** @type {NotificationOptions} */
+  const options = {
+    actions: [
+      {
+        action: 'explore',
+        title: 'Explore this new world',
+      },
+      {
+        action: 'close',
+        title: 'Close notification',
+      },
+    ],
+    body,
+    icon: '/pwa-hello-world/assets/gpui/icon-512.png',
+  };
+  sw.registration.showNotification(title, options);
+}
+
+sw.addEventListener('activate', (event) => {
+  console.log('[SW] Activate');
+  sw.clients.claim();
+  showNotification('Activated!');
+});
+
+sw.addEventListener('notificationclick', (event) => {
+  const { action, notification } = event;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    sw.clients.openWindow('http://www.example.com');
+    notification.close();
+  }
+});
+
+/**
+ * @param {RequestInfo} key
+ */
+async function loadCache (key) {
+  const cache = await sw.caches.open('pwa-hello-world');
+  return await cache.match(key) || new Response('', { status: 404 });
+}
+
+sw.addEventListener('fetch', (event) => {
+  const sUrl = event.request.url;
+  // console.log('[SW] Fetch', sUrl);
+
+  const url = new URL(sUrl);
+  if (url.pathname === '/pwa-hello-world/') {
+    const p = loadCache(url.pathname);
+    event.respondWith(p);
+  }
+});
+
+sw.addEventListener('message', async (event) => {
+  const message = event.data;
+
+  switch (message.type) {
+    case 'sw/skipWaiting': {
+      sw.skipWaiting();
+      break;
+    }
+
+    default: // do nothing;
+  }
+});
+
+// sw.addEventListener('push', (event) => {
+//   const p = showNotification('Pushed!');
+//   event.waitUntil(p);
+// });
